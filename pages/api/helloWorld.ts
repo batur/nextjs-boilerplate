85d7688cad7504b3448741020e96d6266fc9361e
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-08-27 00:51:29 +0300

    feat(app): Add client page and root layout

--- pages/api/helloWorld.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-interface ResponseData {
-  message: string;
-}
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>): void {
-  res.status(200).json({ message: 'Hello from Next.js!' });
-}
