a93372a22c07e5c2db9fe501a5b97a656e4c5e62
Batur <halidbaturakkurt@gmail.com>
2024-09-18 00:32:17 +0300

    refactor: core components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/core/components';
+
 const Home = () => {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button variant='secondary'>Click me</Button>
     </div>
   );
 };
