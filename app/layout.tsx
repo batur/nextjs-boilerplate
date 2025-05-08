5c01972d04f7653b742d9910570080b0e5f9b733
Batur <halidbaturakkurt@gmail.com>
2025-05-08 14:56:05 +0300

    feat: tanstack query for api req

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../styles/globals.scss';
+import { Providers } from '@components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export * from './ui';
+export { default as Providers } from './Providers';
